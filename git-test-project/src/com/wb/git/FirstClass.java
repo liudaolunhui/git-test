bf6521203936937b5364592b04beecd0ae94024d
Moss Kate <1321057554@qq.com>
2020-03-23 19:45:11 +0800

    本地工程第一次添加

---
+++ git-test-project/src/com/wb/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.wb.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("第一个git 的test");
+        System.out.println("第一个git 的test");
+        System.out.println("第一个git 的test");
+        System.out.println("第一个git 的test");
+        System.out.println("第一个git 的test");
+
+
+    }
+}
